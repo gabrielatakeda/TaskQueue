649d310c7cba2b33ce84191741a9f0467d18a793
JLMartinazzo <jl.martinazzo@gmail.com>
2024-10-21 20:05:56 -0300

    remocao func javascript

--- script.js
+++
@@ -1,9 +0,0 @@
-function PassaCima(id){
-    let botao = document.getElementById(`${id}`)
-    botao.style = "background-color: red;"
-}
-
-function SaiCima(id){
-    let botao = document.getElementById(`${id}`)
-    botao.style = "background-color: blue;"
-}
