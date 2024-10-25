26d123711c71d6615f7975732871ada1d542daf2
JLMartinazzo <jl.martinazzo@gmail.com>
2024-10-24 21:49:44 -0300

    criação de boards

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let boards = []
 
 
-document.getElementById("botaoCriar").addEventListener("click", function criar(evento){
+document.getElementById("botaoCriar").addEventListener("click", function aparecer(evento){
     let divBoardForm  = document.getElementById("formNewBoard");
 
     if(divBoardForm.classList.contains("hide")){
@@ -12,10 +12,45 @@ document.getElementById("botaoCriar").addEventListener("click", function criar(e
         divBoardForm.classList.add("hide")
     }
 
-
-
-   /* let boardName = document.getElementById("inputBoard").value
-    boards.push(boardName)
-    console.log(boards);
-    document.getElementById("inputBoard").value = ""*/
 })
+
+const form = document.querySelector("form");
+var cont = 0
+
+
+form.addEventListener(
+    "submit",
+    (event) => {
+        const data = new FormData(form);
+        let output = "";
+        for (const entry of data) {
+            output = `${entry[1]}\r`;
+        }
+    event.preventDefault();
+
+    let board = document.createElement("div")
+    board.classList.add("Quadrado")
+    
+    board.id = "quadrado"+cont
+
+    console.log(board.id)
+
+    var teste = document.getElementById("teste")
+
+    var filho = document.getElementById("filho")
+
+    teste.insertBefore(board, filho)
+
+    var selectedBoard = document.getElementById("quadrado"+cont)
+    
+    selectedBoard.style.backgroundColor = output
+    selectedBoard.innerHTML = output
+
+    cont++
+
+    
+
+
+    }
+
+)
